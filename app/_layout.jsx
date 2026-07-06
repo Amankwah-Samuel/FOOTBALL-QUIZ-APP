e77505c89adf2b99ec255031fb6778802f6b400d
Samuel-Amankwah <your-new-account-email@gmail.com>
2026-07-06 11:52:43 +0000

    changes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,30 +1,29 @@
-import { StyleSheet, Text, View, useColorScheme } from 'react-native'
-import React from 'react'
-import { Stack} from 'expo-router'
-import Colors from '../constants/colors'
-import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, useColorScheme } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+import Colors from "../constants/colors";
+import { StatusBar } from "expo-status-bar";
 
 const RootLayout = () => {
-
-    const colorScheme = useColorScheme()
-    const theme = Colors[colorScheme] ?? Colors.light
+    const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
   return (
-    
     <>
-    <StatusBar value= "auto"/>
-     <Stack screenOptions={{
-headerStyle:{backgroundColor:theme.background},
-headerTintColor:theme.text
-        }}>
-            <Stack.Screen name="index" options={{title:"Home"}}/>
-            <Stack.Screen name="about" options={{title:"About page"}}/>
-            <Stack.Screen name="contact" options={{title:"Contact us"}}/> 
-        </Stack>
+      <StatusBar value="auto" />
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: theme.background },
+          headerTintColor: theme.text,
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen name="about" options={{ title: "About page" }} />
+        <Stack.Screen name="contact" options={{ title: "Contact us" }} />
+      </Stack>
     </>
-       
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
 
-const styles = StyleSheet.create({}) 
+const styles = StyleSheet.create({});
